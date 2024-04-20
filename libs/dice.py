7a3930c176da531f57b5ae3d893b71e430460911
dymastery <dymastery@outlook.com>
2024-04-20 23:39:23 +0200

    Renew Dice Class

--- libs/dice.py
+++ libs/dice.py
@@ -1,6 +1,18 @@
+import random
+
 # 骰子类定义
 class Dice:
-    """定义骰子的类，变量d代表骰子面数"""
+    """
+    定义骰子的类：
+    face: 骰子面数
+    `d_example = Dice(6)`: 创建一个六面骰
+    `d_example()`: 骰d6的值
+    """
     # 实例化
-    def __init__(self, d):
-        self.d = d
+    def __init__(self, face):
+        self.__face = face
+    
+    # 投骰子函数
+    def __call__(self):
+        return random.randint(1, self.__face)
+    
